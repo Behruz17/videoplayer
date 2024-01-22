3bdd68cc67e2a4d690f113484e675c964b017b8c
behruz17 <sultonovjon17@gmail.com>
2024-01-22 23:27:40 +0500

    fixed video links and added loader

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,14 +1,33 @@
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, ActivityIndicator, TouchableOpacity} from 'react-native';
 import Video from 'react-native-video';
 
+
 export default function VideoPlayer({src}) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoadStart = () => {
+    setIsLoading(true);
+  };
+
+  const handleLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
     <View style={styles.container}>
+      {isLoading && (
+        <View style={styles.loaderContainer}>
+          <ActivityIndicator size="large" color="#875eec" />
+        </View>
+      )}
       <Video
         controls
         resizeMode="cover"
         source={{uri: src}}
         style={styles.backgroundVideo}
+        onLoadStart={handleLoadStart}
+        onLoad={handleLoad}
       />
     </View>
   );
@@ -17,6 +36,12 @@ export default function VideoPlayer({src}) {
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
+  },
+  loaderContainer: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.9)',
   },
   backgroundVideo: {
     width: 340,
